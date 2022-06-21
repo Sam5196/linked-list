7839b13c7955a49abd54948fc40bf2a191bd854e
Sam5196 <sammedpachore1008@gmail.com>
2022-06-21 19:59:09 +0530

    Welcome to Linked List Program

---
+++ src/com/bridgelabz/Node.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+
+    public class Node<K> {
+        public int data;
+        public Node next;
+        public Node(int data) {
+            super();
+            this.data = data;
+            this.next = null;
+        }
+    }
